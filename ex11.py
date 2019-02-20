19a19d5a5bd3fe59047226d22a8077768aa959f2
begsener <47365482+begsener@users.noreply.github.com>
2019-02-20 12:30:57 +0300

    Delete ex11.py

--- ex11.py
+++
@@ -1,8 +0,0 @@
-#exercise 11
-print ("How old are you?"),
-age = input()
-print ("How tall are you?"),
-height = input()
-print ("How much do you weight?"),
-weight = input()
-print ("So, you're %r years old, %r tall and %r heavy." % (age, height, weight))
